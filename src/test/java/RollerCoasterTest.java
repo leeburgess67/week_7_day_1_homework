d45e6eed0dcf08b252c4963f024837b36ecc71f6
leeburgess67 <leeburgessmail@gmail.com>
2018-07-02 19:18:15 +0100

    Rollercoaster and dodgem tests passing

--- src/test/java/RollerCoasterTest.java
+++ src/test/java/RollerCoasterTest.java
@@ -1,2 +1,26 @@
+import Attractions.RollerCoaster;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class RollerCoasterTest {
+
+    RollerCoaster rollerCoaster;
+
+    @Before
+    public void before(){
+        rollerCoaster = new RollerCoaster("Loop da loop", 9);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Loop da loop", rollerCoaster.getName());
+    }
+
+    @Test
+    public void canGetRating(){
+        assertEquals(9, rollerCoaster.getRating());
+    }
+
 }
